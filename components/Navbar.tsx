df9b900d2d1e9cc172eb736fd606cb78cd490727
rxqvx <henriquekenji12@gmail.com>
2022-07-06 16:35:50 -0300

    tentando arrumar o build pt 6

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -13,21 +13,26 @@ const Navbar = () => {
 
   return (
     <nav className={styles.navbar}>
-      <div className={styles.logo}>
-        <Image
-          src="/images/pokeball.png"
-          alt="pokeball"
-          width={30}
-          height={30}
-        />
-        <h1>PokeNext</h1>
-      </div>
+      <Link href='/'>
+        <a>
+          <div className={styles.logo}>
+            <Image
+              src="/images/pokeball.png"
+              alt="pokeball"
+              width={30}
+              height={30}
+            />
+            <h1>PokeNext</h1>
+          </div>
+        </a>
+      </Link>
       <Flex gap={4}>
         <Text fontSize='lg' alignSelf="center" fontFamily='sans-serif'>Pesquise pelo Pokemon:</Text>
         {/* erro no build no input do chakraui<Input value={pokemon} focusBorderColor="#E33D33" onChange={(e: any) => handleChange(e.target.value)} size='md' width="auto" color="tomato" placeholder="ex.:Pikachu" _placeholder={{ opacity: 0.4, color: 'inherit' }} /> */}
         {/* erro no build no input do antd <Input placeholder="pikachu" onChange={(e: any) => handleChange(e.target.value)} value={pokemon} onPressEnter={handleSearch} /> */}
-        <input onChange={(e: any) => handleChange(e.target.value)} placeholder="pikachu" className={styles.input} />
-        <Button onClick={handleSearch} colorScheme='red' variant='outline'>Buscar</Button>
+        <input onChange={(e: any) => handleChange(e.target.value)} placeholder="pikachu" className={styles.input} value={pokemon} />
+        {/* <Button onClick={handleSearch} colorScheme='red' variant='outline'>Buscar</Button> */}
+        <button onClick={handleSearch} className={styles.button_search}>Buscar</button>
       </Flex>
       <ul className={styles.link_items}>
         <li>
