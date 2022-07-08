12537506dc42bac46944e8557e1921080830d59b
rxqvx <henriquekenji12@gmail.com>
2022-07-08 09:39:26 -0300

    feat: responsive navbar

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,22 +1,58 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import styles from '../styles/Card.module.css'
+import { Image, Flex, Text } from '@chakra-ui/react'
 
 const Card = ({ pokemon }) => {
     return (
-        <div className={styles.card}>
+        <Flex
+            justifyContent='center'
+            alignItems='center'
+            flexDirection='column'
+            padding='2em 1em'
+            marginBottom='2em'
+            width={['80%', '23%']}
+            borderRadius='1em'
+            border='2px solid #E33D33'
+            boxShadow='5px 5px 12px rgba(0, 0, 0, 0.5)'
+            color='#fff'
+            backgroundColor='#333'
+        // margin={5}
+        >
             <Image
                 src={`https://cdn.traction.one/pokedex/pokemon/${pokemon.id}.png`}
                 width="120"
                 height="120"
                 alt={pokemon.name}
             />
-            <p className={styles.id}>#{pokemon.id}</p>
-            <h3 className={styles.title}>{pokemon.name}</h3>
-            <Link href={`/pokemon/${pokemon.id}`}>
-                <a className={styles.btn}>Detalhes</a>
-            </Link>
-        </div >
+            <Text
+                as="p"
+                margin='1em 0'
+                backgroundColor='#e33d33'
+                borderRadius='5px'
+                padding='5px'
+                color='#fff'
+                display='flex'
+                justifyContent='center'
+                alignItems='center'
+            >#{pokemon.id}</Text>
+            <Text
+                as="h3"
+                textTransform='capitalize'
+                marginBottom='1em'
+                fontSize='1.4em'
+            >{pokemon.name}</Text>
+            <Text
+                as="a"
+                href={`/pokemon/${pokemon.id}`}
+                textDecoration='none'
+                backgroundColor='#fff'
+                color='#222'
+                padding='.7em 1.2em'
+                borderRadius='5px'
+                fontWeight='bold'
+                transition='0.4s'
+            >
+                Detalhes
+            </Text>
+        </Flex >
     );
 }
 
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -10,7 +10,7 @@ const Layout = ({ children }) => {
         <title>PokeNext</title>
       </Head>
       <Navbar />
-      <main className="main-container">{children}</main>
+      <main className="main-container" >{children}</main>
       <Footer />
     </>
   );
